c82a1ceada096796bef26d39134d98a528c8b6fb
Lunaphied <lunaphied@lunaphied.me>
2026-02-08 10:55:15 +0100

    initial commit

---
+++ package.nix
@@ -0,0 +1,28 @@
+{
+  stdenv,
+  autoPatchelfHook,
+  requireFile,
+  writeShellScript,
+}:
+stdenv.mkDerivation {
+  pname = "wch-toolchain";
+  version = "230";
+
+  src = requireFile {
+    name = "MRS_Toolchain_Linux_x64_V230.tar.xz";
+    url = "https://www.mounriver.com/download";
+    hash = "sha256-YjKlKGbp7UJc7Wg4+ZtGEm+CHZqJbHZlebeQ2OHu3lQ=";
+    #description = "Go to the Linux section and download the toolchain specifically.";
+  };
+
+  builder = writeShellScript "extractor.sh" ''
+    tar -xvf $src "Toolchain/RISC-V Embedded GCC12"
+    mv "Toolchain/RISC-V Embedded GCC12/" $out
+    autoPatchelf $out
+  '';
+
+  buildInputs = [ stdenv.cc.cc ];
+
+  nativeBuildInputs = [ autoPatchelfHook ];
+
+}
